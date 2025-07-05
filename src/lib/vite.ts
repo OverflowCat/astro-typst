271fa26aca80d45e665afc7f76cfb2ef88302e55
OverflowCat <overflowcat@gmail.com>
2025-07-06 05:26:40 +0800

    fix: ignore emitSvg for HTML target

--- src/lib/vite.ts
+++ src/lib/vite.ts
@@ -41,46 +41,7 @@ export default function (config: AstroTypstConfig, options: AstroConfig): Plugin
         load(id) {
             if (!isTypstFile(id)) return;
             debug(`[vite-plugin-astro-typ] Loading id: ${id}`);
-            // const { path, opts } = extractOpts(id);
-            // this.addWatchFile(path);
         },
-
-        // handleHotUpdate(ctx: HmrContext) {
-        //     // ctx.file: 发生变化的文件路径
-        //     // ctx.modules: 受影响的 ModuleNode 数组
-        //     if (!isTypstFile(ctx.file)) return;
-        //     console.log(`File changed: ${ctx.file}`);
-        //     // const importersToUpdate = new Set<any>(); // Use a Set to avoid duplicates
-        //     ctx.modules.forEach((moduleNode: any) => {
-        //         console.log(`  Module affected: ${moduleNode.id || moduleNode.file}`);
-        //         if (moduleNode.importers.size > 0) {
-        //             console.log(`    Imported by:`);
-        //             // @ts-ignore
-        //             moduleNode.importers.forEach((importerNode) => {
-        //                 console.log(`     - ${importerNode.id || importerNode.file}`);
-        //                 // importersToUpdate.add(importerNode);
-        //                 server.moduleGraph.invalidateModule(importerNode);
-        //                 console.log(`    Invalidating module: ${importerNode.id || importerNode.file}`);
-        //             });
-        //         } else {
-        //             console.log(`    Not imported by any other loaded module.`);
-        //         }
-        //     });
-
-        // 返回需要 HMR 的模块，或空数组执行自定义处理/全页面重载
-        // @ts-ignore
-        /*             if (importersToUpdate.size > 0) {
-                        for (const importer of importersToUpdate) {
-                            // const module = server.moduleGraph.getModuleById(importer.id || importer.file);
-                            // if (module) server.moduleGraph.invalidateModule(module);
-                            // else console.log(`  No module found for importer: ${importer.id || importer.file}`);
-                        }
-                        console.log(`  Importers to update: ${Array.from(importersToUpdate).map((m) => m.id || m.file).join(', ')}`);
-                        return Array.from(importersToUpdate);
-                    } */
-        //     ctx.server.hot.send({ type: 'full-reload' });
-        //     return [];
-        // },
 
         configureServer(_server) {
             server = _server;
@@ -97,7 +58,7 @@ export default function (config: AstroTypstConfig, options: AstroConfig): Plugin
                         debug(`[vite-plugin-astro-typ] No modules found for file: ${filePath}`);
                         server.ws.send({
                             type: 'full-reload',
-                            path: '*', // 触发全页面刷新
+                            path: '*',
                         });
                     }
                 }
@@ -124,13 +85,13 @@ export default function (config: AstroTypstConfig, options: AstroConfig): Plugin
             let { html, getFrontmatter } = await renderToHTMLish(
                 {
                     mainFilePath,
-                    body: isBody,
+                    body: emitSvg ? true : isBody,
                 },
                 config.options,
                 isHtml
             );
 
-            if (emitSvg) {
+            if (emitSvg && !isHtml) {
                 let imgSvg = "";
                 const contentHash = crypto.randomUUID().slice(0, 8);
                 const fileName = `typst-${contentHash}.svg`;
@@ -145,7 +106,7 @@ export default function (config: AstroTypstConfig, options: AstroConfig): Plugin
                     publicUrl,
                 })
 
-                if (import.meta.env.PROD) {
+                if (import.meta.env.PROD) { // 'build' mode
                     const emitName = path.join(normalizedSvgDirName, fileName);
                     const respId = this.emitFile({
                         type: 'asset',
@@ -154,9 +115,7 @@ export default function (config: AstroTypstConfig, options: AstroConfig): Plugin
                     });
                     logger.debug("emitFile", respId)
                     imgSvg = `<img src='${publicUrl}' />`;
-                } else { // 'serve' 模式
-                    // use data inline
-                    // imgSvg = `<img src='${publicUrl}' />`;
+                } else { // 'serve' mode inlines svg as base64
                     imgSvg = `<img src="data:image/svg+xml;base64,${Buffer.from(html, 'utf-8').toString('base64')}" />`;
                 }
                 html = imgSvg;
